0e843524882e11eae5cfe362506b53c7dcc3a929
dieisongularte <dieissongularte@gmail.com>
2021-06-14 11:26:15 -0300

    add metodos CombustivelController

--- app/Http/Controllers/CombustivelController.php
+++ app/Http/Controllers/CombustivelController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CalculoDeCombustivel;
 use Illuminate\Http\Request;
 
 class CombustivelController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('consumo');
+    }
+
+    public function gasto()
+    {
+        $calculoDeCombustivel = new CalculoDeCombustivel();
+        $valor = $calculoDeCombustivel->calcular();
+
+        return view('resultado', ['valor'=>$valor]);
+    }
 }
