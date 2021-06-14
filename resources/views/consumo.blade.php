74dca368a590a0222b472dfa6ac34936800674f5
dieisongularte <dieissongularte@gmail.com>
2021-06-14 20:13:05 -0300

    update views

--- resources/views/consumo.blade.php
+++ resources/views/consumo.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Calculo de Consumo de Combustível</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
   </head>
   <body>
     <main>
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Calculo de Consumo de Combustível</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
   </head>
   <body>
     <div class="painel">
