83928a3b2a8111e17c01cbdeaa575f2b8c76b1c9
dieisongularte <dieissongularte@gmail.com>
2021-06-15 10:05:15 -0300

    templete padrao criado

--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -1,24 +1,20 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Calculo de Consumo de Combustível</title>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
-  </head>
-  <body>
-    <div class="painel">
-      <h2>Resultado do cálculo de consumo</h2>
-      <div class="conteudo-painel">
-        <div class="sucesso">
-          <h3>O valor total gasto será de</h3>
-          <ul>
-            <li><b>Gasolina:</b> R$ {{$valor}}</li>
-          </ul>
-        </div>
-        <a class="botao" href="{{url('/')}}" title="Voltar">Voltar</a>
+@extends('app')
+
+@section('titulo')
+Calculo de Consumo de Combustível
+@stop
+
+@section('conteudo')
+  <div class="painel">
+    <h2>Resultado do cálculo de consumo</h2>
+    <div class="conteudo-painel">
+      <div class="sucesso">
+        <h3>O valor total gasto será de</h3>
+        <ul>
+          <li><b>Gasolina:</b> R$ {{$valor}}</li>
+        </ul>
       </div>
+      <a class="botao" href="{{url('/')}}" title="Voltar">Voltar</a>
     </div>
-  </body>
-</html>
+  </div>
+@stop
